e0f2f167d2feb1a7098906f5597d08d308862621
ulfarj <ulfarj@gmail.com>
2018-03-24 22:26:54 +0000

    get shoppers

--- api/models/shopper.js
+++ api/models/shopper.js
@@ -7,9 +7,10 @@ var ShopperSchema = new Schema(
     first_name: {type: String, required: true, max: 100},
     last_name: {type: String, required: true, max: 100},    
     email: {type: String, required: true, max: 100},
+    // location
     price: {type: Number},
     about: {type: String},
-    instagram: {type: String},
+    instagram: {type: String},    
     // styles: {type: []},
     // assists_with:
     //availability     
--- api/shopper.js
+++ api/shopper.js
@@ -3,17 +3,24 @@ var Shopper = require('./models/shopper');
 module.exports = function(app) {
       
   app.post('/api/shopper', (req, res) => {     
-
     var shopper = new Shopper (req.body.shopper);    
     shopper.save(
       function (err) {
-        if (err) {          
-           console.log ('Error on save', err);
-           res.status(500).send(err)
+        if (err) {
+           res.status(500).send(err);
         }
-        console.log('New Shopper: ' + shopper);
         res.send({ shopper });
       }
     );    
   });
+
+  app.get('/api/shopper', (req, res) => {
+    Shopper.find({}, 'price about', function (err, shoppers) {
+      if (err) { 
+        res.status(500).send(err)
+      }
+      res.send({ shopper });
+    });
+  })
+
 };
