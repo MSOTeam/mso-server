8e5a6df48513b5a462bfb5866b3b2595efe56e88
ulfarj <ulfarj@gmail.com>
2018-09-20 21:27:31 +0000

    auth

--- passport/index.js
+++ passport/index.js
@@ -1,4 +1,23 @@
+const passport = require('passport');
 const GoogleTokenStrategy = require('passport-google-token').Strategy;
+
+const User = require('../models/user');
+const config = require('../config/auth');
+
+module.exports = function () {
+
+    passport.use(new GoogleTokenStrategy({
+            clientID: config.googleAuth.clientID,
+            clientSecret: config.googleAuth.clientSecret
+        },
+        function (accessToken, refreshToken, profile, done) {
+            User.upsertGoogleUser(accessToken, refreshToken, profile, function(err, user) {
+                return done(err, user);
+            });
+        }
+    ));
+};
+
 
 // var passport = require('passport');
 // const LocalStrategy = require('passport-local').Strategy;
