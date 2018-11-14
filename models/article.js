fca0af312de6592eb4a387efd438c24ac6bffd36
ulfarj <ulfarj@gmail.com>
2018-11-14 20:48:48 +0000

    added url date_created

--- models/article.js
+++ models/article.js
@@ -1,22 +1,39 @@
 const mongoose = require('mongoose');
+const Tag = require('./tag');
 
 var Schema = mongoose.Schema;
 
 var ArticleSchema = new Schema(
   {
-    user: {type: String},            
+    user: {type: String, required: true},            
     title: {type: String},
     content: { type: String },
     length: { type: String },
     excerpt: { type: String },
     byline: { type: String },
     dir: { type: String },
-    tags: { type: Array },     
+    tags: { type: Array },
+    url: { type: String },     
   },
   { 
     timestamps: true, 
   }
 );
+
+ArticleSchema.pre('save', (next) => {
+
+  next();
+  // const client = this;    
+  // if (!client.isModified('password')) { return next(); }
+  // bcrypt.genSalt(10, (err, salt) => {
+  //   if (err) { return next(err); }
+  //   bcrypt.hash(client.password, salt, null, (err, hash) => {
+  //     if (err) { return next(err); }
+  //     client.password = hash;
+      
+  //   });
+  // });
+});
 
 module.exports = mongoose.model('Article', ArticleSchema);
 
--- routes/article.js
+++ routes/article.js
@@ -18,6 +18,8 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res, next
     const article = new Article(scrapedArticle);
     article.user = req.user._id;
     article.tags = JSON.parse(req.body.tags);
+    article.url = req.body.url;
+    
     article.save(
       function (err) {
         if (err) {        
@@ -30,7 +32,7 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res, next
 });
 
 router.get('/', passport.authenticate('jwt', {session: false}), (req, res, next) => {  
-  Article.find({user: req.user._id}, 'title content length excerpt tags', function (err, articles) {
+  Article.find({user: req.user._id}, 'title content length excerpt tags createdAt url', function (err, articles) {
     if (err) {
       res.status(500).send(err)
     }    
@@ -39,7 +41,7 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req, res, next)
 });
 
 router.get('/:id', passport.authenticate('jwt', {session: false}), (req, res, next) => {  
-  Article.findOne({_id: req.params.id}, 'title content length tags', function (err, article) {
+  Article.findOne({_id: req.params.id}, 'title content length tags createdAt url', function (err, article) {
     if (err) {
       res.status(500).send(err)
     }    
