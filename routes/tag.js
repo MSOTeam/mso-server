8f4afc0049b79ac694e8e72d5e32ed911b070c7a
ulfarj <ulfarj@gmail.com>
2019-05-15 22:41:46 +0000

    get tags

---
+++ routes/tag.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const jsdom = require("jsdom");
+const passport = require('passport');
+
+const router  = express.Router();
+
+const Tag = require('../models/tag');
+
+router.get('/', passport.authenticate('jwt', {session: false}), (req, res, next) => {  
+    Tag.find({user: req.user._id}, 'tag', (err, tags) => {    
+        if (err) {
+            res.status(500).send(err)
+        }    
+        res.send({ tags });    
+    });
+});
