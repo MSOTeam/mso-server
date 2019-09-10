7c6efd055c8368db2cb0e81d0fffb68ed6e46f1c
ulfarj <ulfarj@gmail.com>
2019-09-10 21:18:26 +0000

    deploy

--- index.js
+++ index.js
@@ -41,9 +41,9 @@ app.set('socketio', io);
 
 
 io.on("connection", socket => {
-  console.log("New client connected");
+  // console.log("New client connected");
   socket.on("disconnect", () => {
-    console.log("Client disconnected");
+    // console.log("Client disconnected");
   });
 });
 
